a9674c35d17e3babebd3447a21efa17969765d84
Fenneig <ninetailedkot92@gmail.com>
2023-08-31 11:40:49 +0300

    Add dotween, TextMeshPro and floating text

---
+++ Assets/Scripts/Components/Environment/Target/TargetComponent.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using DEEPP.Components.UI.GameObjectsUI;
+using UnityEngine;
+
+namespace DEEPP.Components.Environment.Target
+{
+    public class TargetComponent : MonoBehaviour
+    {
+        [SerializeField] private Canvas _pointsShowCanvas;
+        [SerializeField] private FloatingTextComponent _pointsTextComponentPrefab;
+        [SerializeField] private List<NumericalColor> _numericalColors;
+
+        private void Awake()
+        {
+            _pointsShowCanvas.worldCamera = Camera.main;
+        }
+
+        public void ShowScore(int score)
+        {
+            Color targetColor = Color.white;
+            foreach (var numericalColor in _numericalColors)
+            {
+                if (score >= numericalColor.MinValue) targetColor = numericalColor.MatchingColor;
+                else break;
+            }
+            var pointsText = Instantiate(_pointsTextComponentPrefab, _pointsShowCanvas.transform);
+            pointsText.AnimateText(score.ToString(), targetColor);
+        }
+    }
+
+    [Serializable]
+    public class NumericalColor
+    {
+        [field: SerializeField] public int MinValue { get; private set; }
+        [field: SerializeField] public Color MatchingColor { get; private set; }
+    }
+}
---
+++ Assets/Scripts/Components/Environment/Target/TargetPointsComponent.cs
@@ -0,0 +1,15 @@
+﻿using DEEPP.Components.Weapon;
+using UnityEngine;
+
+namespace DEEPP.Components.Environment.Target
+{
+    public class TargetPointsComponent : MonoBehaviour, IDamageable
+    {
+        [SerializeField] private TargetComponent _targetComponent;
+        [SerializeField] private int _score;
+        public void Damage()
+        {
+            _targetComponent.ShowScore(_score);
+        }
+    }
+}
